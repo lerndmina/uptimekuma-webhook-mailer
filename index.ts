00b58cde984100b0003f328c556370f2ec790df1
Adam <wild@lerndmina.dev>
2025-01-23 16:49:15 +0000

    Enhance Uptime Kuma webhook handling with email templating and improved error responses

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import nodemailer from "nodemailer";
-import { isUptimeKumaWebhook } from "./types";
+import { isUptimeKumaWebhook, type UptimeKumaMonitor, type UptimeKumaWebhook } from "./types";
+import fs from "fs";
+import path from "path";
+import { Liquid } from "liquidjs";
+import { log } from "console";
 
 const creds = {
   user: process.env.EMAIL_USER,
@@ -40,22 +44,7 @@ function main() {
       }
 
       // Return a 400 for any other request
-      return new Response(
-        `
-Bad request
-
-Usage:
-- POST /webhook?token=secret-token
-
-Parameters:
-- token: The webhook token to authorize the request
-
-Data:
-- The request body should be a JSON matching the format of an uptimekuma webhook.
-
-        `,
-        { status: 400 }
-      );
+      return new Response("Bad request\n" + help, { status: 400 });
     },
   });
 }
@@ -78,8 +67,10 @@ async function sendMail(req: Request): Promise<Response> {
     }
   } catch (e) {
     console.error("Invalid JSON data:", e);
-    return new Response("Invalid JSON data\n" + (e instanceof Error ? e.toString() : String(e)), { status: 400 });
+    return new Response("Invalid JSON data\n" + (e instanceof Error ? e.toString() : String(e)) + `\n${help}`, { status: 400 });
   }
+
+  console.log("Sending email with data:", data);
 
   // Request is valid, send the email
   const transporter = nodemailer.createTransport({
@@ -94,14 +85,9 @@ async function sendMail(req: Request): Promise<Response> {
   const mailOptions = {
     from: creds.from,
     to: creds.to,
-    subject: "Uptimekuma Webhook",
-    text: JSON.stringify(data, null, 2),
-    html: `
-<h1>Uptimekuma Webhook</h1>
-<pre>
-${JSON.stringify(data, null, 2)}
-</pre>
-    `,
+    subject: data.msg,
+    text: getPlainTextEmail(data),
+    html: await getEmailTextFromFile(data),
   };
 
   await transporter.sendMail(mailOptions);
@@ -113,4 +99,76 @@ ${JSON.stringify(data, null, 2)}
     `,
     { status: 200 }
   );
+}
+
+async function getEmailTextFromFile(data: UptimeKumaWebhook): Promise<string> {
+  // Data is stored in email.html in this directory read in the string and return it
+  const filePath = path.join(__dirname, "email.html");
+  const rawData = fs.readFileSync(filePath, "utf8");
+  const engine = new Liquid();
+  const tpl = engine.parse(rawData);
+  const html = await engine.render(tpl, data);
+  return html;
+}
+
+function getPlainTextEmail(data: UptimeKumaWebhook): string {
+  const status = data.heartbeat.status === 0 ? "DOWN" : "UP";
+  return `
+MONITOR ALERT: ${data.monitor.name}
+===============================
+Status: [${status}]
+Time: ${data.heartbeat.localDateTime}
+Duration: ${data.heartbeat.duration} seconds
+Message: ${data.heartbeat.msg}
+
+Monitor Details:
+---------------
+URL: ${data.monitor.url}
+Type: ${data.monitor.type}
+Retries: ${data.heartbeat.retries}
+Priority: ${data.heartbeat.important ? "High" : "Normal"}
+
+Timezone Info: ${data.heartbeat.timezone} (${data.heartbeat.timezoneOffset})
+
+Looks like your email client doesn't support HTML emails. For a better experience, please enable HTML emails so we can send you fancy emails in the future.
+`;
+}
+
+const help = `
+Usage:
+- POST /webhook?token=secret-token
+
+Parameters:
+- token: The webhook token to authorize the request
+
+Data:
+- The request body should be a JSON matching the format of an uptimekuma webhook.
+
+Example:
+${JSON.stringify(getExampleData(), null, 2)}
+`;
+
+function getExampleData(): UptimeKumaWebhook {
+  return {
+    heartbeat: {
+      monitorID: 1,
+      status: 0,
+      time: "2024-01-23 16:18:51.838",
+      msg: "No heartbeat in the time window",
+      important: true,
+      duration: 61,
+      retries: 1,
+      timezone: "Europe/London",
+      timezoneOffset: "+00:00",
+      localDateTime: "2024-01-23 16:18:51",
+    },
+    monitor: {
+      id: 1,
+      name: "Example Monitor",
+      url: "https://example.com",
+      type: "push",
+      active: true,
+    } as UptimeKumaMonitor,
+    msg: "[Example Monitor] [🔴 Down] No heartbeat in the time window",
+  };
 }
